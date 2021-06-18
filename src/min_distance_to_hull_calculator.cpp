fb375da45c1f65e162750d73f303ca785ea69545
Heruka Andradi <heruka.andradi@locomotec.com>
2021-06-18 18:41:10 +0200

    Added cavity detector and classifier nodes

--- src/min_distance_to_hull_calculator.cpp
+++ src/min_distance_to_hull_calculator.cpp
@@ -1,8 +1,10 @@
-#include <ppt_3d/min_distance_to_hull_calculator.hpp>
+#include <min_distance_to_hull_calculator.hpp>
 
 #include <math.h>
 #include <limits>
 #include <iostream>
+
+typedef pcl::PointXYZRGBA PointRGBA;
 
 MinDistanceToHullCalculator::MinDistanceToHullCalculator()
 {
@@ -13,17 +15,17 @@ MinDistanceToHullCalculator::~MinDistanceToHullCalculator()
 {
 }
 
-void MinDistanceToHullCalculator::setConvexHullPointsAndEdges(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& convex_hull_cloud)
+void MinDistanceToHullCalculator::setConvexHullPointsAndEdges(const pcl::PointCloud<PointRGBA>::ConstPtr& convex_hull_cloud)
 {   
     hull_points_.clear();
     normalized_hull_edge_vecs_.clear();
-    const pcl::PointXYZ* pcl_pt;
+    const PointRGBA* pcl_pt;
     Eigen::Vector3f hull_edge_pt_1, hull_edge_pt_2, edge_vec;
     pcl_pt = &convex_hull_cloud->points[0];
     hull_edge_pt_1 << pcl_pt->x, pcl_pt->y, pcl_pt->z;
     for( size_t i = 0; i < convex_hull_cloud->points.size(); i++){
       pcl_pt = &convex_hull_cloud->points[i];
-      hull_edge_pt_1 << pcl_pt->x, pcl_pt->y, pcl_pt->z;
+      hull_edge_pt_2 << pcl_pt->x, pcl_pt->y, pcl_pt->z;
       hull_points_.push_back(hull_edge_pt_1);
       edge_vec = hull_edge_pt_2 - hull_edge_pt_1;
       normalized_hull_edge_vecs_.push_back(edge_vec/edge_vec.norm());
@@ -38,7 +40,7 @@ float MinDistanceToHullCalculator::computeMinDistanceToEdge(Eigen::Vector3f& pt,
     return pow((pow(edge_orig_to_pt_vec.norm(), 2) - pow(normalized_edge_vec.dot(edge_orig_to_pt_vec), 2) ), 0.5);
 }
 
-float MinDistanceToHullCalculator::computeMinDistanceToHull(const pcl::PointXYZ& object_point)
+float MinDistanceToHullCalculator::computeMinDistanceToHull(const PointRGBA& object_point)
 {
     Eigen::Vector3f obj_pt;
     float pt_to_edge_dist, min_pt_to_edge_dist = std::numeric_limits<float>::max();
@@ -57,7 +59,7 @@ float MinDistanceToHullCalculator::computeMinDistanceToHull(const pcl::PointXYZ&
     return min_pt_to_edge_dist;
 }
 
-float MinDistanceToHullCalculator::computeMinDistanceToHull(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& object_cloud)
+float MinDistanceToHullCalculator::computeMinDistanceToHull(const pcl::PointCloud<PointRGBA>::ConstPtr& object_cloud)
 {
     float min_pt_to_edge_dist, min_obj_to_hull_dist = std::numeric_limits<float>::max();
     if (convex_hull_available_)
@@ -71,6 +73,6 @@ float MinDistanceToHullCalculator::computeMinDistanceToHull(const pcl::PointClou
     {
         std::cout << "!!! The convex hull points of the workspace are yet to be set !!!" << std::endl;
     }
-    return min_pt_to_edge_dist;
+    return min_obj_to_hull_dist;
 }
 
